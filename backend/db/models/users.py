8cc12de86010c3b377d3a63d68e7edb77e9ee118
Taufiq Ismail <taufikikk@gmail.com>
2021-12-13 11:22:52 +0700

    section 2 code

---
+++ backend/db/models/users.py
@@ -0,0 +1,15 @@
+from re import S
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
+from sqlalchemy.orm import relation, relationship
+
+from db.base_class import Base
+
+
+class User(Base):
+    id = Column(Integer,primary_key=True,index=True)
+    username = Column(String,unique=True,nullable=False)
+    email = Column(String,nullable=False, unique=True,index=True)
+    hashed_password = Column(String,nullable=False)
+    is_active = Column(Boolean(), default=True)
+    is_superuser = Column(Boolean(),default=False)
+    jobs = relationship("Job",back_populates="owner")
--- backend/db/session.py
+++ backend/db/session.py
@@ -2,6 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 from core.config import settings
+from typing import Generator
 
 # SQLALCHEMY_DATABASE_URL = settings.DATABASE_URL
 # engine = create_engine(SQLALCHEMY_DATABASE_URL)
@@ -13,3 +14,10 @@ engine = create_engine(
 
 
 SessionLocal = sessionmaker(autocommit = False, autoflush=False, bind=engine)
+
+def get_db() -> Generator: 
+    try:
+        db = SessionLocal()
+        yield db
+    finally:
+        db.close()
